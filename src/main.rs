70f39bdfffdf31a682243d68cfe0432025747714
Raui Ghazaleh <rj.ghazaleh@gmail.com>
2020-09-07 20:33:00 +0200

    Implemented a better fibonacci calculator

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,11 @@
-use std::process::exit;
-
 fn main() {
-
     let nth_digit: i32 = 100;
-    let mut f_0: i128 = 1;  // f_n
-    let mut f_1 = 1;  // f_n+1
-    let mut index = 2;
-    println!("{}", 1);
-    println!("{}", 1);
-
-    if nth_digit == 1 || nth_digit == 2 {
-        println!("1");
-        exit(0);
+    let mut a:i128 = 0;
+    let mut b = 1;
+    for _ in 0..nth_digit {
+        println!("{}", b);
+        let tmp = a;
+        a = b;
+        b = a + tmp;
     }
-    while index < nth_digit {
-        let tmp = f_0;
-        f_0 = f_1;
-        f_1 = tmp + f_1;
-        index += 1;
-        println!("{}", f_1)
-    }
-
-
 }

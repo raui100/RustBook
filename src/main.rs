6eeb6c08a8af0ba4948742594ed276c8dadafca8
Raui Ghazaleh <rj.ghazaleh@gmail.com>
2020-09-11 09:28:41 +0200

    Calculating an obscene big fibonacci number

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,19 @@
-struct Fibonacci(u128, u128);
+use num::BigUint;
+use num::{Zero, One};
+use std::mem::replace;
 
-fn next_f(f: &Fibonacci) -> Option<Fibonacci> {
-    let f_1 = f.1.checked_add(f.0);
-    match f_1 {
-        None => None,
-        Some(f_1) => Some(Fibonacci(f.1, f_1))
+// Calculates large fibonacci numbers.
+fn fib(n: u128) -> BigUint {
+    let mut f0: BigUint = Zero::zero();
+    let mut f1: BigUint = One::one();
+    for _ in 0..n {
+        let f2 = f0 + &f1;
+        // This is a low cost way of swapping f0 with f1 and f1 with f2.
+        f0 = replace(&mut f1, f2);
     }
+    f0
 }
 
 fn main() {
-    let mut f = Some(Fibonacci(0, 1));
-    loop {
-        match &f {
-            None => break,
-            Some(fib) => {
-                println!("{}", fib.0);
-                f = next_f(&fib);
-            }
-        }
-    }
+    println!("fib({}) = {}", 100000, fib(100000));
 }

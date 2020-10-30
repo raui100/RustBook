f2a218bef27288019f652d35c05bca786a02c76c
Raui Ghazaleh <rj.ghazaleh@gmail.com>
2020-10-30 15:08:10 +0100

    Tried moving the "_" pattern to the top of the match. This blocks all following arms for comparison and produces warnings.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,11 @@
-use num::BigUint;
-use num::{Zero, One};
-use std::mem::replace;
-
-// Calculates large fibonacci numbers.
-fn fib(n: u128) -> BigUint {
-    let mut f0: BigUint = Zero::zero();
-    let mut f1: BigUint = One::one();
-    for _ in 0..n {
-        let f2 = f0 + &f1;
-        // This is a low cost way of swapping f0 with f1 and f1 with f2.
-        f0 = replace(&mut f1, f2);
-    }
-    f0
-}
-
 fn main() {
-    println!("fib({}) = {}", 100000, fib(100000));
+    let some_u8_value: u8 = 1;
+    match some_u8_value {
+        _ => (),
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+
+    }
 }
